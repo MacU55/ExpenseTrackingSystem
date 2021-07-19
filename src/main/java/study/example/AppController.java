25344df93611e06605cebeee02d282bf352c1b9b
Max <51697067+MacU55@users.noreply.github.com>
2021-07-19 14:14:23 +0300

    add writing runtime events into external log file ets.log

--- src/main/java/study/example/AppController.java
+++ src/main/java/study/example/AppController.java
@@ -1,6 +1,6 @@
 package study.example;
 
-import org.apache.log4j.RollingFileAppender;
+//import org.apache.log4j.RollingFileAppender;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
