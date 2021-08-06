2fbcc805872bd65f00dfae69b5509b224435ae42
Max <51697067+MacU55@users.noreply.github.com>
2021-08-06 22:46:01 +0300

    make first approach security. created registration and authorization for users. It's need to adjust expense list and make user's behavior for it's expenses maintenance only.

--- src/main/java/study/example/repository/UserRepository.java
+++ src/main/java/study/example/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package study.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import study.example.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    public User findByEmail(String email);
+
+    //User findByEmail(String email);
+    //User findByUserName(String userName);
 }
